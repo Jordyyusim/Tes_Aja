91fc8032b66c6d4710716cdecc022c0666132cab
Jordyyusim <jordyzenith@gmail.com>
2020-02-13 12:08:44 +0700

    lagu

---
+++ b.py
@@ -0,0 +1,6 @@
+print('Twinkle Twinkle Little Star,' 
+'\n' '\t' 'How I wonder what you are!' 
+'\n' '\t' '\t' 'Up above the world so high'
+'\n' '\t' '\t' 'Like a diamond in the sky.'
+'\n''Twinkle Twinkle Little Star,'
+'\n' '\t' 'How I wonder what you are!')
